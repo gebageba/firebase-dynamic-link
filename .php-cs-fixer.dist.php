b131f06fa92941f9a5b1c90cd57b9dc9a2f9e8f1
gebageba <nagaikanae34@gmail.com>
2022-12-28 16:08:36 +0900

    Fix: header コメント

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -52,7 +52,7 @@ return $config
         'header_comment' => [
             'comment_type' => 'comment',
             'header' => <<<HEREDOC
-This file is part of Firebase-Dynamic-Login.
+This file is part of Firebase-Dynamic-Link.
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 HEREDOC,
